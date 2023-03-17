05811bec57079e3ab694d56ea907c5a42a96a0a5
nicolasgoetschel <nicgoe@hotmail.com>
2023-03-17 22:56:32 +0000

    lists_task_start.py added

---
+++ week_01/day_2/lists_task_start.py
@@ -0,0 +1,12 @@
+# 1. Create an empty list called `task_list`
+task_list = []
+# 2. Add a few `str` elements, representing some everyday tasks e.g. 'Make Dinner'
+task_list = "make dinner", "clean", "hover"
+# 3. Print out `task_list`
+print(task_list)
+# 4. Remove the last task
+task_list.pop()
+print(task_list)
+# 5. Print out `task_list`
+
+# 6. Print out the number of elements in `task_list`
