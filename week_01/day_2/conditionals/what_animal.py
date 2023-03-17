171d2f133c4e708d820046699f6cece138b40956
nicolasgoetschel <nicgoe@hotmail.com>
2023-03-17 22:49:06 +0000

    what_animal.py added

---
+++ week_01/day_2/conditionals/what_animal.py
@@ -0,0 +1,8 @@
+animal = input("What's the best animal? ")
+
+if animal.lower() == "elephant":
+    print("This is my fav animal!")
+elif animal.lower() == "penguin":
+    print("That's my second fav!")
+else:
+    print("Sad, not my fav!")     
